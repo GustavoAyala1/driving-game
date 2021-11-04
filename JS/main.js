a253e9b96c215e78b2b68cefaca6b53540eb345e
Gustavo Ayala <gayalaand@gmail.com>
2021-11-04 15:56:07 -0700

    changing direction now stops other directions from moving

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,40 @@
 const $car = document.querySelector(".car");
 
+let left = 0;
+let topandbottom = 0;
+
+let start;
+
 const handleTurn = (event) => {
   const key = event.key;
-  console.log(key);
   if (key === "ArrowUp" || key === "w") {
     $car.className = "car left";
+    clearInterval(start);
+    start = setInterval(() => {
+      topandbottom -= 1;
+      $car.style.top = `${topandbottom}px`;
+    }, 16);
   } else if (key === "ArrowLeft" || key === "a") {
     $car.className = "car reverse";
+    clearInterval(start);
+    start = setInterval(() => {
+      left -= 1;
+      $car.style.left = `${left}px`;
+    }, 16);
   } else if (key === "ArrowDown" || key === "s") {
     $car.className = "car right";
+    clearInterval(start);
+    start = setInterval(() => {
+      topandbottom += 1;
+      $car.style.top = `${topandbottom}px`;
+    }, 16);
   } else if (key === "ArrowRight" || key === "d") {
     $car.className = "car forward";
+    clearInterval(start);
+    start = setInterval(() => {
+      left += 1;
+      $car.style.left = `${left}px`;
+    }, 16);
   }
 };
 
