d69854fa72e9cc79352726c7da9ef912cec2d2a2
Gustavo Ayala <gayalaand@gmail.com>
2021-11-05 13:22:24 -0700

    user can stop the car

--- JS/main.js
+++ JS/main.js
@@ -3,42 +3,51 @@ const $car = document.querySelector(".car");
 let left = 0;
 let topandbottom = 0;
 let moving = "stopped";
+let direction;
 
 let start;
 
 const handleTurn = (event) => {
   const key = event.key;
-  const direction = key.slice(5).toLowerCase();
-  if (moving !== "stopped") {
+  const spaceKey = event.code;
+  if (spaceKey !== "Space") {
+    direction = key.slice(5).toLowerCase();
+  }
+
+  if (spaceKey === "Space") {
     moving = "stopped";
     $car.className = `car ${direction}`;
     clearInterval(start);
-  } else if (key === "ArrowUp" || key === "w") {
+  } else if (key === "ArrowUp") {
     moving = "running";
     $car.className = "car up";
+    clearInterval(start);
     start = setInterval(() => {
-      topandbottom -= 1;
+      topandbottom -= 2;
       $car.style.top = `${topandbottom}px`;
     }, 16);
-  } else if (key === "ArrowLeft" || key === "a") {
+  } else if (key === "ArrowLeft") {
     moving = "running";
     $car.className = "car left";
+    clearInterval(start);
     start = setInterval(() => {
-      left -= 1;
+      left -= 2;
       $car.style.left = `${left}px`;
     }, 16);
-  } else if (key === "ArrowDown" || key === "s") {
+  } else if (key === "ArrowDown") {
     moving = "running";
     $car.className = "car down";
+    clearInterval(start);
     start = setInterval(() => {
-      topandbottom += 1;
+      topandbottom += 2;
       $car.style.top = `${topandbottom}px`;
     }, 16);
-  } else if (key === "ArrowRight" || key === "d") {
+  } else if (key === "ArrowRight") {
     moving = "running";
     $car.className = "car right";
+    clearInterval(start);
     start = setInterval(() => {
-      left += 1;
+      left += 2;
       $car.style.left = `${left}px`;
     }, 16);
   }
