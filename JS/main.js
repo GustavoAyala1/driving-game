91f9cab8cbe485950927102bcdd7fd094bd27b0a
Gustavo Ayala <gayalaand@gmail.com>
2021-11-04 17:26:10 -0700

    determine whether the car is moving and is facing / if pressing the direction it is facing then start moving

--- JS/main.js
+++ JS/main.js
@@ -2,35 +2,41 @@ const $car = document.querySelector(".car");
 
 let left = 0;
 let topandbottom = 0;
+let moving = "stopped";
 
 let start;
 
 const handleTurn = (event) => {
   const key = event.key;
-  if (key === "ArrowUp" || key === "w") {
-    $car.className = "car left";
+  const direction = key.slice(5).toLowerCase();
+  if (moving !== "stopped") {
+    moving = "stopped";
+    $car.className = `car ${direction}`;
     clearInterval(start);
+  } else if (key === "ArrowUp" || key === "w") {
+    moving = "running";
+    $car.className = "car up";
     start = setInterval(() => {
       topandbottom -= 1;
       $car.style.top = `${topandbottom}px`;
     }, 16);
   } else if (key === "ArrowLeft" || key === "a") {
-    $car.className = "car reverse";
-    clearInterval(start);
+    moving = "running";
+    $car.className = "car left";
     start = setInterval(() => {
       left -= 1;
       $car.style.left = `${left}px`;
     }, 16);
   } else if (key === "ArrowDown" || key === "s") {
-    $car.className = "car right";
-    clearInterval(start);
+    moving = "running";
+    $car.className = "car down";
     start = setInterval(() => {
       topandbottom += 1;
       $car.style.top = `${topandbottom}px`;
     }, 16);
   } else if (key === "ArrowRight" || key === "d") {
-    $car.className = "car forward";
-    clearInterval(start);
+    moving = "running";
+    $car.className = "car right";
     start = setInterval(() => {
       left += 1;
       $car.style.left = `${left}px`;
